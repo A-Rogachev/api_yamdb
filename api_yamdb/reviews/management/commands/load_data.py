11eafd1c7c2657354c4857587fb6a2142ce07089
A-Rogachev <97498636+A-Rogachev@users.noreply.github.com>
2023-02-07 22:50:30 +0400

    2nd iteration

--- api_yamdb/reviews/management/commands/load_data.py
+++ api_yamdb/reviews/management/commands/load_data.py
@@ -1,10 +1,13 @@
 import csv
 import os
+import sys
 from typing import Dict
 
 from django.conf import settings
 from django.core.management import BaseCommand
+from django.db import IntegrityError
 from django.db.models import Model
+
 from reviews.models import Category, Comment, Genre, Review, Title
 from users.models import User
 
@@ -24,24 +27,51 @@ data_for_database: Dict[Model, str] = {
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         for db_model, file_and_args in data_for_database.items():
-            data_file = open(file_and_args[0], 'r', encoding='utf-8')
-            objects_queue = []
-            reader = csv.DictReader(
-                data_file,
-                delimiter=',',
-                quotechar='"',
-                skipinitialspace=True,
-            )
-            # случай, когда необходимо переопределить значения в аргументах
-            if not file_and_args[1]:
-                for row in reader:
-                    objects_queue.append(db_model(**row))
-            else:
-                for row in reader:
-                    data_args: Dict[str, str] = dict(**row)
-                    for key, value in file_and_args[1].items():
-                        data_args[key] = value.objects.get(pk=data_args[key])
-                    objects_queue.append(db_model(**data_args))
+            try:
+                with open(
+                    file_and_args[0], 'r', encoding='utf-8'
+                ) as data_file:
+                    objects_queue = []
+                    reader = csv.DictReader(
+                        data_file,
+                        delimiter=',',
+                        quotechar='"',
+                        skipinitialspace=True,
+                    )
+                    for row in reader:
+                        data_args: Dict[str, str] = dict(**row)
+                        if file_and_args[1]:
+                            for key, value in file_and_args[1].items():
+                                data_args[key] = value.objects.get(
+                                    pk=data_args[key]
+                                )
+                        objects_queue.append(db_model(**data_args))
+                    db_model.objects.bulk_create(objects_queue)
 
-            db_model.objects.bulk_create(objects_queue)
-            data_file.close()
+            except FileNotFoundError:
+                self.stdout.write(
+                    self.style.ERROR(
+                        f'Файла {file_and_args[0]} нет в рабочем каталоге!'
+                        '\nРабота загрузчика прервана!'
+                    )
+                )
+                sys.exit()
+            except IntegrityError:
+                self.stdout.write(
+                    self.style.ERROR(
+                        f'Oшибка при работе с файлом{file_and_args[0]}'
+                        '\nРабота загрузчика прервана!'
+                    )
+                )
+                sys.exit()
+            else:
+                self.stdout.write(
+                    self.style.SUCCESS(
+                        f'Данные из файла {file_and_args[0]} успешно загружены'
+                    )
+                )
+        self.stdout.write(
+            self.style.SUCCESS(
+                'Работа загрузчика завершена успешно!'
+            )
+        )
